3509e61cad22c9425166d54876c76f2fc7c86240
Sara-ibrahiim <saraibrahim.0260@gmail.com>
2025-09-02 22:08:22 +0300

    Fiximage

--- components/features/Features.tsx
+++ components/features/Features.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 const Features = () => {
   return (
     <section id="features" className="scroll-mt-24  md:ps-15">
-      <div className="py-[20px] md:p-[120px] max-w-7xl mx-auto">
+      <div className=" md:px-[60px] md:py-[80px] max-w-7xl mx-auto">
         <div className="grid grid-cols-12 gap-[2px]">
           <div className="md:bg-[#111] flex items-center justify-center h-[300px] col-span-12 md:col-span-5">
             <Image
-              src="/AI/work.svg"
+              src="/AI/work.png"
               alt="work"
               width={400}
               height={400}
@@ -17,7 +17,7 @@ const Features = () => {
           </div>
           <div className="md:bg-[#111] flex items-center justify-center h-[300px] col-span-12 md:col-span-3">
             <Image
-              src="/AI/accese.svg"
+              src="/AI/accese.png"
               alt="accese"
               width={400}
               height={400}
@@ -26,7 +26,7 @@ const Features = () => {
           </div>
           <div className="md:bg-[#111] flex items-center justify-center h-[300px] col-span-12 md:col-span-3">
             <Image
-              src="/AI/build.svg"
+              src="/AI/build.png"
               alt="build"
               width={400}
               height={400}
@@ -35,7 +35,7 @@ const Features = () => {
           </div>
           <div className="md:bg-[#111] flex items-center justify-center h-[300px] col-span-12 md:col-span-3">
             <Image
-              src="/AI/feat.svg"
+              src="/AI/feat.png"
               alt="feat"
               width={400}
               height={400}
@@ -44,7 +44,7 @@ const Features = () => {
           </div>
           <div className="md:bg-[#111] flex items-center justify-center h-[300px] col-span-12 md:col-span-3">
             <Image
-              src="/AI/product.svg"
+              src="/AI/product.png"
               alt="product"
               width={400}
               height={400}
@@ -53,7 +53,7 @@ const Features = () => {
           </div>
           <div className="md:bg-[#111] flex items-center justify-center h-[300px] col-span-12 md:col-span-5">
             <Image
-              src="/AI/inbox.svg"
+              src="/AI/inbox.png"
               alt="inbox"
               width={400}
               height={400}
