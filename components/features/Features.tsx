f995aa42d96252f4526f5cb5c968c07f693117b5
Sara-ibrahiim <saraibrahim.0260@gmail.com>
2025-09-02 21:17:40 +0300

    border

--- components/features/Features.tsx
+++ components/features/Features.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Features = () => {
   return (
-    <section className="scroll-mt-24  md:ps-15">
+    <section id="features" className="scroll-mt-24  md:ps-15">
       <div className="py-[20px] md:p-[120px] max-w-7xl mx-auto">
         <div className="grid grid-cols-12 gap-[2px]">
           <div className="md:bg-[#111] flex items-center justify-center h-[300px] col-span-12 md:col-span-5">
